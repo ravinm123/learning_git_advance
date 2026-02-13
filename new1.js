96556edc4f6ce605d069cad5c7de86e8666cee7f
Ravi Munavalli (EXT-Nokia) <ravi.munavalli.ext@nokia.com>
2026-02-12 22:37:42 -0700

    upadated new feature

---
+++ new1.js
@@ -0,0 +1,17 @@
+import { useState,useEffect } from "react" 
+const news = ()=>{
+
+    const [open ,setOpen]=useState(false)
+
+    const fetchdata = ()=>{
+        const data  = 
+            `${inport.meta.env.vite}\'url'`
+            Headers:{
+                method:get
+                content:
+                authentication:token
+            }
+        
+    }
+
+}  
